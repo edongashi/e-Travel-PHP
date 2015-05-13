f55bd8aee00311c2d1c2611d10b562819d47263e
EdonGashi <edongashi@hotmail.co.uk>
2015-05-13 15:49:59 +0200

    Dashboard

--- resources/config.php
+++ resources/config.php
@@ -26,12 +26,37 @@ $config = array(
         ),
         "Lokacionet" => "lokacionet.php",
         "Kontakti" => "kontakti.php"
+    ),
+    "dashboard_links_admin" => array(
+        "Ballina" => "index.php",
+        "Rezervimet" => array(
+          "Rezervim i ri" => "rezervimi.php",
+          "Shfaq rezervimet" => "shfaq_rezervimet.php"
+        ),
+        "Regjistro udhetime" => array(
+            "Autobus" => "regjistro_bus.php",
+            "Aeroplan" => "regjistro_aeroplan.php",
+        ),
+        "Shto lokacion" => "regjistro_lokacione.php",
+        "Menaxho databazen" => "menaxhoDB.php"
+    ),
+    "dashboard_links_user" => array(
+        "Ballina" => "index.php",
+        "Udhetimet" => array(
+            "Autobus" => "udhetimet_bus.php",
+            "Aeroplan" => "udhetimet_aeroplan.php",
+        ),
+        "Lokacionet" => "lokacionet.php",
+        "Kontakti" => "kontakti.php"
     )
 );
 
 defined("templates") or define("templates", $_DIR . "/templates");
 defined("library") or define("library", $_DIR . "/library");
+defined("databaza") or define("databaza", $_DIR . "/library/createDB.php");
 defined("templates_header") or define("templates_header", $_DIR . "/templates/header.php");
 defined("templates_footer") or define("templates_footer", $_DIR . "/templates/footer.php");
+defined("dashboard_header") or define("dashboard_header", $_DIR . "/templates/dashboard_header.php");
+defined("dashboard_footer") or define("dashboard_footer", $_DIR . "/templates/dashboard_footer.php");
 
 ?>
