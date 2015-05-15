4ff3ff33c813e9308cfbacd882f3cbb202416531
bleendd <bleendd.1@gmail.com>
2015-05-15 16:27:41 +0200

    Lokacionet Koment
    Koment per secilin lokacion tbazum ne ID te lokacionet me metoden GET.

--- public_html/lokacionet.php
+++ public_html/lokacionet.php
@@ -2,7 +2,7 @@
 require_once("../resources/config.php");
 
 $header_titulli = "Ballina";
-$css_includes = "css/site.css";
+$css_includes = Array("../css/form.css", "../css/site.css");
 require(templates_header);
 require(databaza);
 
@@ -12,23 +12,35 @@ require(databaza);
     
 	$repo = new repository();
 	$rows = $repo->lokacionet;
+	if ( !isset($_GET["id"]))
+	{
     foreach ($rows as $rreshti)
 		{
             echo "<p><img src='img/content/".$rreshti['Foto']."' height='150px' width='150px'><b> ".$rreshti['Vendi']."</b><br />".$rreshti['Pershkrimi']."</p>";
-        
-		include("menaxhimi/regjistro_komentet.php");
-		$rows_forum = $repo->forumi;
-
-        foreach ( $rows_forum as $rreshti_forum)
-		{
-			echo '<br>' . '<ul>' .$rreshti_forum["Komentuesi"];
-            echo '<div class="koment_head">' .$rreshti_forum['Komenti'] . '</div> </ul><br>';
-        }
-
+			echo "
+			<form method='GET' action=''>
+			<button type='submit' name='id' value=" . $rreshti['Lid'] .">Vazhdo</button>
+			</form>";
 		}
- 
-	
+	}
+	else
+	{
+		$_SESSION['ID'] = $_GET['id'];
+		$rreshti = $repo->get_data("SELECT * FROM lokacione WHERE Lid=" . $_GET['id'])[0];
+		echo "<p><img src='img/content/". $rreshti['Foto'] ."' height='150px' width='150px'><b> ".$rreshti['Vendi']."</b><br />".$rreshti['Pershkrimi']."</p>";
+		
+		include("menaxhimi/regjistro_komentet.php");
+		
+		$rreshti_forum = $repo->get_data("SELECT * FROM forumi WHERE ChatID=" . $_GET["id"]);
+		foreach ( $rreshti_forum as $rreshti)
+		{
+			echo '<br><div class="form"><span class="tabela">' .$rreshti['Komentuesi'];
+			echo '</span><br><div class="tabela">' .$rreshti['Komenti'] . '</div> </ul></form>';
+		}
+	}
+		
 	?>
+	
 
 </section>
 
