e662d99fb1b3b247981ca2bb5a9328b7da91ef50
EdonGashi <edongashi@hotmail.co.uk>
2015-06-03 23:11:22 +0200

    Finalizimi :clap:

--- public_html/menaxhimi/regjistro_fotoGaleri.php
+++ public_html/menaxhimi/regjistro_fotoGaleri.php
@@ -9,30 +9,31 @@ require(dashboard_header);
 
 <section class="permbajtje">
 
- <h1 class="center">Shto foto ne galeri:</h1>
- <form class="form" action="file_upload.php" method="post" enctype="multipart/form-data">
-    <table>
-        <?php if (isset($error_msg)) echo "<tr><td colspan='2'><h4 class='error-msg'>$error_msg</h4></td></tr>"; ?>
-        <tr>
-            <td>Foto:</td>
-            <td>
-			    <input type="file" name="fileToUpload" id="fileToUpload">
-			</td>
-        </tr>
-	    <tr>
-            <td>Numri rendor:</td>
-			<td>
-                <input type="number" name="numri_rendor">
-			</td>
-        </tr>
-		<tr>
-			<td class="button-cell-small">
-				<input class="button button-small" type="submit" value="Upload Image" name="submit">
-                <input class="button button-small" type="reset" value="Anulo">
-            </td>
-		</tr>
-    </table>			    
-</form>
+    <h1 class="center">Shto foto ne galeri</h1>
+    <form class="form" style="max-width: 500px;" action="file_upload.php" method="post" enctype="multipart/form-data">
+        <table>
+            <?php if (isset($error_msg)) echo "<tr><td colspan='2'><h4 class='error-msg'>$error_msg</h4></td></tr>"; ?>
+            <tr>
+                <td>Foto:</td>
+                <td>
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                </td>
+            </tr>
+            <tr>
+                <td>Numri rendor:</td>
+                <td>
+                    <input style="margin-top: 5px;" type="number" name="numri_rendor">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td class="button-cell-small">
+                    <input class="button button-small" type="submit" value="Upload Image" name="submit">
+                    <input class="button button-small" type="reset" value="Anulo">
+                </td>
+            </tr>
+        </table>
+    </form>
 
 </section>
 
